75e5799238ad3b0cf508d7863c6fd204964e4a23
Idan Weinstein <idanwe@mellanox.com>
2012-03-21 14:47:12 +0000

    though we are not suppose to use anymore the switch_hostname script, we are still using it sometimes so i fix it to support the new 'r-' hostname prefix of our Zorro cluster

--- scripts/switch_hostname.sh
+++ scripts/switch_hostname.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-base_h=$(hostname | sed 's/-.*//')
+base_h=$(hostname | sed 's/r-/r\*/g' | sed 's/-.*//g' | sed 's/r\*/r-/')
 
 if [ -z ${1} ]; then
 	echo "Switching host name to ${base_h}"
