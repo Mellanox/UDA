186fdbe3f871b37b83a0feaee70dab3e8cdc3a04
Avner Ben Hanoch <avnerb@mellanox.com>
2012-05-31 15:53:18 +0000

    The plugin (java & c++) and rpm build is even more amazing than ever...

--- rpm/buildall.sh
+++ rpm/buildall.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
+cd `dirname $0`
+UDA_DIR=`pwd`/..
+
 #prepare C++
 cd ../src
-./autogen.sh /volt/avnerb/workspace/uda/hadoop-1.1.0-uda/
+#./autogen.sh $UDA_DIR/hadoop-1.1.0-uda/
+./autogen.sh $UDA_DIR/hadoop-1.0.2-uda/
 autoreconf --install
 ./configure
 cd -
@@ -12,8 +16,13 @@ cd -
 #prepare JAVA
 FILE=makefile.mk
 cd ../mlx
-echo 'CBASE=/volt/avnerb/workspace/uda/hadoop-1.1.0-uda/build/hadoop-1.1.0-SNAPSHOT/' > $FILE
-echo 'CPATH=.:$(CBASE)/hadoop-core-1.1.0-SNAPSHOT.jar:$(CBASE)/lib/commons-logging-1.1.1.jar' >> $FILE
+#for 1.1
+#echo "CBASE=$UDA_DIR/hadoop-1.1.0-uda/build/hadoop-1.1.0-SNAPSHOT/" > $FILE
+#echo "CPATH=.:\$(CBASE)/hadoop-core-1.1.0-SNAPSHOT.jar:\$(CBASE)/lib/commons-logging-1.1.1.jar" >> $FILE
+
+#for 1.0
+echo "CBASE=$UDA_DIR/hadoop-1.0.2-uda/build/UDA-2.1.0-HADOOP-1.0.2/" > $FILE
+echo "CPATH=.:\$(CBASE)/hadoop-core-1.0.2.jar:\$(CBASE)/lib/commons-logging-1.1.1.jar" >> $FILE
 cd -
 
 #build C++ and JAVA, and then create RPM
