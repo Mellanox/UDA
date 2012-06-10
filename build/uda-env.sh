21448e08d9c23c900c6ce166696912711ac3aa77
Avner Ben Hanoch <avnerb@mellanox.com>
2012-06-10 09:08:21 +0000

    useful content

--- build/uda-env.sh
+++ build/uda-env.sh
@@ -1,3 +1,5 @@
 #
 #this file should be sourced into other bash script/environment
 #
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/lib64/uda/uda.jar
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/uda/
