12c79818ba50a96ba400da9778aa6aa41d423095
Katya Katsenelenbogen <katyak@mellanox.com>
2012-06-28 08:23:58 +0000

    This script updates property "slave.host.name" at mapred-site.xml. It calculates the value according to the host. It can either update it to be the ip of the desired interface or string containing hostname with appropriate ending (such as "r-zorro002-ib") depending on a passed parameter

---
+++ scripts/set_slave_host_name.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Written by Katya Katsenelenbogen 
+# Date: 2012-06-19
+
+if [ -z "$3" ] 
+then
+	echo "Usage: you must pass 
+	*	desired mode(ip or hostname).
+	*	the desired interface for ip, or the desired ending for hostname
+			For example ib0 ip OR eth4 hostname
+	* 	full path to desired xml file"
+	exit 1;
+fi
+filename=$3
+element_name="slave.host.name"
+interface=$2
+
+
+old_property_raw=`cat $filename | grep -A 4 $element_name `
+old_property=`echo $old_property_raw | awk 'BEGIN {FS="</property>"} {print $1}'`
+old_value=`echo $old_property | awk 'BEGIN {FS="value>"} {print $2}' | cut -f1 -d "<"` 
+
+echo "the current value for the element <$element_name> is '$old_value'"
+
+if [ $1 == "ip" ]
+then
+	new_value=`ifconfig $interface 2>/dev/null | grep inet | cut -f2 -d ":" | cut -f1 -d " "`
+elif [ $1 == "hostname" ]
+then
+	new_value=`hostname`-$interface
+else
+		echo "mode should be 'ip' or 'hostname'. you passed $1"
+		exit 1;
+fi
+
+awk -v new_value=$new_value -v key=$element_name -v old_value=$old_value '
+BEGIN {
+        keyFound=0
+		value_found=0
+}
+{
+	if (key_found==0 && value_found==0){ #searching for "slave.host.name"
+		key_found=match($0, key)
+		value_found=match($0, "value")
+		if (value_found)
+			sub(old_value, new_value)
+	}
+	if (value_found==0) {
+		value_found=match($0, "value")
+		if (value_found)
+			sub(old_value, new_value)
+	}
+	print
+}
+' $filename > ${filename}.new
+
+mv  ${filename}.new ${filename}
+rm -rf ${filename}.new
+
+exit 0;
