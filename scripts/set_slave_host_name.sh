dd751358023e6e865c351952aa2fa600b9fe64cb
Katya Katsenelenbogen <katyak@mellanox.com>
2012-07-02 12:41:40 +0000

    beautify script: 1. it now adds property "slave.host.name" if it does not appear in xml file. 2. example of parameters passed: hadoop_conf_dir --interface=eth4 OR hadoop_conf_dir --host_suffix=ib

--- scripts/set_slave_host_name.sh
+++ scripts/set_slave_host_name.sh
@@ -2,35 +2,97 @@
 # Written by Katya Katsenelenbogen 
 # Date: 2012-06-19
 
-if [ -z "$3" ] 
+
+
+usage() {
+	echo "
+usage: $0 <options>
+	There are two ways to configure 'slave.host.name' property:
+		1. Use a suffix to be added to hostname (such as 'eagle1-ib'). In this
+			case in /etc/hosts should be an entry spesifying ip for 'eagle1-ib'.
+		2. Use ip address of the interface you wish to run hadoop on.
+	
+	Required options:
+		--uda-hadoop-conf-dir=DIR  	path to existing hadoop installation that will use hadoop
+	AND one of the following options:
+		--interface=INTERFACE_NAME	interface name as it appears in 'ifconfig'
+		--host_suffix=HOST_SUFFIX  	suffix to be added to hostname
+  "
+  exit 1
+}
+
+
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'uda-hadoop-conf-dir:' \
+  -l 'host_suffix:' \
+  -l 'interface:' \
+  -- "$@")
+
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --uda-hadoop-conf-dir)
+        HADOOP_CONF_DIR=$2 ; shift 2
+        ;;
+        --interface)
+        INTERFACE_NAME=$2 ; shift 2
+        ;;
+        --host_suffix)
+        HOST_SUFFIX=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+
+if [ -z "$(eval "echo \$HADOOP_CONF_DIR")" ]; 
 then
-	echo "Usage: you must pass 
-	*	desired mode(ip or hostname).
-	*	the desired interface for ip, or the desired ending for hostname
-			For example ib0 ip OR eth4 hostname
-	* 	full path to desired xml file"
-	exit 1;
+    echo Missing param: hadoop-conf-dir
+    usage
 fi
-filename=$3
+
+if [ -n "$HOST_SUFFIX" ]  
+then
+	new_value=`hostname`-$HOST_SUFFIX
+elif [ -n "$INTERFACE_NAME" ] 
+then 
+	new_value=`ifconfig $interface 2>/dev/null | grep inet | cut -f2 -d ":" | cut -f1 -d " "`
+else
+	usage
+fi
+
+filename=$HADOOP_CONF_DIR/mapred-site.xml
 element_name="slave.host.name"
-interface=$2
 
 
 old_property_raw=`cat $filename | grep -A 4 $element_name `
+
+if [ -z "$old_property_raw" ]
+then
+	echo "property does not exist: adding it!"
+	new_property="<property> \n<name>$element_name <\\\name> \n<value>$new_value<\\\value> \n<\\\property>"
+	lala="chocholate"
+	sed "/^<configuration>/a $new_property" $filename > ${filename}.new
+	mv  ${filename}.new ${filename}
+	rm -rf ${filename}.new
+	exit 1;
+fi
+
+
 old_property=`echo $old_property_raw | awk 'BEGIN {FS="</property>"} {print $1}'`
 old_value=`echo $old_property | awk 'BEGIN {FS="value>"} {print $2}' | cut -f1 -d "<"` 
 
-
-if [ $1 == "ip" ]
-then
-	new_value=`ifconfig $interface 2>/dev/null | grep inet | cut -f2 -d ":" | cut -f1 -d " "`
-elif [ $1 == "hostname" ]
-then
-	new_value=`hostname`-$interface
-else
-		echo "mode should be 'ip' or 'hostname'. you passed $1"
-		exit 1;
-fi
 
 echo "the current value for the element <$element_name> is '$old_value' and the new value is '$new_value'"
 
