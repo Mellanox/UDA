19426c1f00ad9baef4a15701ce3533771ef28d1d
Katya Katsenelenbogen <katyak@mellanox.com>
2012-07-29 13:44:59 +0000

    for the regression

--- build/makerpm.sh
+++ build/makerpm.sh
@@ -6,8 +6,8 @@ cd `dirname $0`
 
 export JAVA_HOME=/usr/lib64/java/jdk1.6.0_25 
 make -C ../src clean all
-make -C ../mlx-CDH3u4 clean all
-make -C ../mlx-0.20.2 clean all
+#make -C ../mlx-CDH3u4 clean all #works, but you have to make sure that CPATH in mlx-CDH3u4/Makefile is correct
+#make -C ../mlx-0.20.2 clean all #works, but you have to make sure that CPATH in mlx-0.20.2/Makefile is correct
 make -C ../mlx-1.x clean all
 
 cp ../src/.libs/libuda.so .
