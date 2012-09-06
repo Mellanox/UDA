2f4e87b10f84ed814d1cc4d291b22a4239596acb
Avner Ben Hanoch <avnerb@mellanox.com>
2012-09-06 07:23:16 +0000

    also create source.tgz (still todo: put it in the RPM)

--- build/buildrpm.sh
+++ build/buildrpm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+cd `dirname $0`
 
 #one time setup per user's home
 if [ ! -f ~/.rpmmacros ] ; then
@@ -8,9 +9,15 @@ if [ ! -f ~/.rpmmacros ] ; then
 fi
 
 #prepare C++
-cd `dirname $0`
+echo ===== preparing C++ make ...
 ./../src/premake.sh
 
+# export UDA into source dir, remove plugins/*/*.jar, and create tarball
+echo ===== Creating source.tgz ...
+rm -rf source.tgz source
+svn export .. source && rm source/plugins/*/*.jar && tar cfz source.tgz source
+
 #build C++ and JAVA, and then create RPM
+echo ===== making RPM ...
 ./makerpm.sh 
 cd -
