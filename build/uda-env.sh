0474656673e96ab7eeeaff652d84b2eb09bc2c25
Avner Ben Hanoch <avnerb@mellanox.com>
2012-09-13 08:12:19 +0000

    remove libuda.so from env

--- build/uda-env.sh
+++ build/uda-env.sh
@@ -2,4 +2,3 @@
 #this file should be sourced into other bash script/environment
 #
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/lib64/uda/uda.jar
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/uda/
