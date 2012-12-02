73022de9a765aec8f971f2785be31585a0576c60
Ori Zaig <oriz@mellanox.com>
2012-12-02 18:38:47 +0200

    removing hadoop 0.20.2 from the daily-regression

--- scripts/regression_v2/performBM.sh
+++ scripts/regression_v2/performBM.sh
@@ -6,10 +6,10 @@ bash $SCRIPTS_DIR/autoTester.sh -bseadcrz \
 -Dcsv='/.autodirect/mtrswgwork/UDA/daily_regressions/configuration_files/regression-1.1.0.csv' \
 -Drpm.jar='uda-hadoop-1.x.jar' 
 
-export TMP_DIR="/tmp/regression-0.20.2"
-bash $SCRIPTS_DIR/autoTester.sh -bseadcrz \
--Dgit.hadoop.version='hadoop-0.20.2-patched-v2' \
--Dcsv='/.autodirect/mtrswgwork/UDA/daily_regressions/configuration_files/regression-0.20.2.csv' \
--Drpm.jar='uda-hadoop-0.20.2.jar' 
+#export TMP_DIR="/tmp/regression-0.20.2"
+#bash $SCRIPTS_DIR/autoTester.sh -bseadcrz \
+#-Dgit.hadoop.version='hadoop-0.20.2-patched-v2' \
+#-Dcsv='/.autodirect/mtrswgwork/UDA/daily_regressions/configuration_files/regression-0.20.2.csv' \
+#-Drpm.jar='uda-hadoop-0.20.2.jar' 
 
 
