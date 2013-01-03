32ab7a2c054f50818a093fbae9380c4c79dce55d
Avner BenHanoch <avnerb@mellanox.com>
2013-01-03 10:18:49 +0200

    a more elegant file with same functionality

--- build/makerpm.sh
+++ build/makerpm.sh
@@ -7,18 +7,14 @@ cd `dirname $0`
 export JAVA_HOME=/usr/lib64/java/jdk1.6.0_25 
 make -C ../
 
-cp ../src/.libs/libuda.so .
-cp ../plugins/mlx-1.x/uda-hadoop-1.x.jar .
-cp ../plugins/mlx-3.x/uda-hadoop-3.x.jar .
-cp ../plugins/mlx-CDH3u4/uda-CDH3u4.jar .
-cp ../scripts/set_hadoop_slave_property.sh .
-
-cp uda-hadoop-1.x.jar uda-hadoop-3.x.jar uda-CDH3u4.jar libuda.so README LICENSE.txt journal.txt set_hadoop_slave_property.sh source.tgz ~/rpmbuild/SOURCES/
+echo ========== creating RPM...
 
 uda_version=`awk -F- '{print $1}' ../release`
 uda_fix=`awk -F- '{print $2}' ../release`
 revision=`awk -F: '{print $1}' gitversion.txt` # remove ':' (in case user's dir is dirty), since it is illegal in spec file
-echo ========== creating RPM...
+
+cp ../plugins/mlx*/uda*.jar ../src/.libs/libuda.so ../scripts/set_hadoop_slave_property.sh  README LICENSE.txt journal.txt source.tgz ~/rpmbuild/SOURCES/
 rpmbuild -ba uda.spec --define "_revision $revision" --define "_uda_version $uda_version" --define "_uda_fix $uda_fix"
+
 cd -
 echo ========== SUCCESS: Your RPM is under: ~/rpmbuild/RPMS/
