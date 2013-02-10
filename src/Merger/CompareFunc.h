f4c084e2d69d240ddf7ddf0b2bb5bd1c40afc2a9
Avner BenHanoch <avnerb@mellanox.com>
2013-02-10 12:07:41 +0200

    remove functions without inline from *.h file since they are included multiple times and cause link errors

--- src/Merger/CompareFunc.h
+++ src/Merger/CompareFunc.h
@@ -28,17 +28,6 @@ typedef int (*hadoop_cmp_func)(char* key1, int len1, char* key2, int len2);
 // set once on init_reduce_task according to Java's reducer's comparator type name
 extern hadoop_cmp_func g_cmp_func;
 
-int byte_compare(char* key1, int len1, char* key2, int len2) {
-    int cmp_res =  memcmp(key1, key2, ((len1 < len2) ? len1 : len2));
-	return (cmp_res) ? (cmp_res) : (len1 - len2);
-}
-
-int java_text_compare(char* key1, int len1, char* key2, int len2) {
-	int k1_skip_bytes = StreamUtility::decodeVIntSize((int)(key1[0]));
-	int k2_skip_bytes = StreamUtility::decodeVIntSize((int)(key2[0]));
-	return byte_compare(key1 + k1_skip_bytes, len1 - k1_skip_bytes, key2 + k2_skip_bytes, len2 - k2_skip_bytes);
-}
-
 hadoop_cmp_func get_compare_func(const char* java_comparator_type_name);
 
 #endif
