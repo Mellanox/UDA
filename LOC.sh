22099e8478b22b460e50ab1cb2aa9ffcaea0285a
Alex Rosenbaum <alexr@mellanox.com>
2013-02-13 10:50:04 +0200

    update LOC (Lines of Code) script to run from remote git url (clean from build objects)

--- LOC.sh
+++ LOC.sh
@@ -3,10 +3,13 @@
 TEAM="Acceleration"
 PROJECT="UDA"
 VERSION="3.1"
-
 EXCLUDES="`pwd`/LOC_exclude_UDA.txt"
-python /.autodirect/mtrswgwork/accounter_lines_of_code/bin/loc_count.py -T $TEAM -P $PROJECT -V $VERSION -E $EXCLUDES -p `pwd`
 
-#REPO_GIT="git:///.autodirect/mswg/git/accl/uda.git"
-#BRANCH="master"
-#python /.autodirect/mtrswgwork/accounter_lines_of_code/bin/loc_count.py -T $TEAM -P $PROJECT -V $VERSION -E $EXCLUDES -R $REPO_GIT -B $BRANCH
+REPO_GIT="git:///.autodirect/mswg/git/accl/uda.git"
+BRANCH="master"
+
+# Run from remote git url (clean from build objects)
+python /.autodirect/mtrswgwork/accounter_lines_of_code/bin/loc_count.py -T $TEAM -P $PROJECT -V $VERSION -E $EXCLUDES -R $REPO_GIT -B $BRANCH
+
+# Run from local path
+#python /.autodirect/mtrswgwork/accounter_lines_of_code/bin/loc_count.py -T $TEAM -P $PROJECT -V $VERSION -E $EXCLUDES -p `pwd`
