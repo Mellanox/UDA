bdde708228a19a9b9db79cfbb9e53d5685d043b7
Albert Chu <chu11@llnl.gov>
2013-03-06 14:01:54 -0800

    src/UdaBridge.cc: Fix invalid debug log message output
    Signed-off-by: Albert Chu <chu11@llnl.gov>

--- src/UdaBridge.cc
+++ src/UdaBridge.cc
@@ -328,13 +328,13 @@ void UdaBridge_invoke_dataFromUda_callback(JNIEnv * jniEnv, jobject jbuf, int le
 }
 
 index_record* UdaBridge_invoke_getPathUda_callback(JNIEnv * jniEnv, const char* job_id, const char* map_id, int reduceId) {
-	log(lsTRACE, "before jniEnv->CallStaticVoidMethod...");
 	jstring jstr_job, jstr_map;
 	jstr_job = jniEnv->NewStringUTF(job_id);
 	jstr_map = jniEnv->NewStringUTF(map_id); //NewStringUTF allocates a string inside the JVM which will release it
+	log(lsTRACE, "before jniEnv->CallStaticObjectMethod...");
 
 	jobject jdata = jniEnv->CallStaticObjectMethod(jclassUdaBridge, jmethodID_getPathUda, jstr_job,  jstr_map, reduceId);
-	log(lsTRACE, "after  jniEnv->CallStaticVoidMethod...");
+	log(lsTRACE, "after  jniEnv->CallStaticObjectMethod...");
 
 	jniEnv->DeleteLocalRef(jstr_job);
 	jniEnv->DeleteLocalRef(jstr_map);
