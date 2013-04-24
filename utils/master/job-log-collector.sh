aead20efa6228b369d5a168816fb1f61296e81d9
Avner BenHanoch <avnerb@mellanox.com>
2013-04-24 15:55:21 +0300

    many, many improvements in log collecting, most of them for cloudera, but not only (still problem to run ./bin/slaves.sh in cloudera distributions)

--- utils/master/job-log-collector.sh
+++ utils/master/job-log-collector.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# job that we want its logs, for example: 201304091252_0008
-JOB=${1-201304091252_0008}
+# job that we want its logs, for example: job_201304091252_0008
+JOB=$1  #for example: job_201304091252_0008}
+
 # where are the logs on the slaves, for example $HADOOP_HOME/logs
-LOGDIR=${2-/var/logs/hadoop}
+LOGDIR=${2:-$HADOOP_LOG_DIR}
+TMP=`ps -ef | grep jobtracker | awk 'BEGIN {RS=" "; FS="="} $1=="-Dhadoop.log.dir" {print $2}'`
+LOGDIR=${LOGDIR:-$TMP}
+TMP=`ps -ef | grep tasktracker | awk 'BEGIN {RS=" "; FS="="} $1=="-Dhadoop.log.dir" {print $2}'`
+LOGDIR=${LOGDIR:-$TMP}
+LOGDIR=${LOGDIR:-/var/logs/hadoop}
+
+
 #how many lines before/after the job to copy from job/task tracker log files
-CONTEXT=${3-1000}
+CONTEXT=${3:-1000}
 
 ####################  just initialization ###
 
@@ -15,15 +23,29 @@ SCRIPTSDIR=`dirname $0`/..
 #tells the slaves were to scp their logs to
 MASTER=`hostname`
 
-PROG=$0
+#calculate HADOOP_HOME, based on jobtracker, or default value - if defined
+MY_HADOOP_HOME=`ps -ef | grep jobtracker | awk 'BEGIN {RS=" "; FS="="} $1=="-Dhadoop.home.dir" {print $2}'`
+HADOOP_HOME=${HADOOP_HOME:-$MY_HADOOP_HOME}
+
+#calculate HADOOP_HOME, based on tasktracker, or previous calculated value - if exists
+MY_HADOOP_HOME=`ps -ef | grep tasktracker | awk 'BEGIN {RS=" "; FS="="} $1=="-Dhadoop.home.dir" {print $2}'`
+HADOOP_HOME=${HADOOP_HOME:-$MY_HADOOP_HOME}
+
+echo $LOGDIR | grep --silent '^/' || LOGDIR=`pwd`/$LOGDIR #relative-path to absolute-path
+JOB=`echo $JOB | sed 's/^job_//'` # trim job_ prefix
+
+
+PROG=`basename $0`
 RM=/bin/rm
 
 echo $PROG: This script will collect the following files from the LOGDIR on each slave:
-echo "$PROG:  * JOB's_conf.xml and JOB's snippets from all *tracker* files"
-echo "$PROG:  * JOB's folder from userlogs dir"
-echo "$PROG:  * optionaly if exist: *tracker*out* files"
+echo "$PROG:  *** JOB's_conf.xml and JOB's snippets from all *tracker* files"
+echo "$PROG:  *** JOB's folder from userlogs dir"
+echo "$PROG:  *** optionaly if exist: *tracker*out* files"
 echo $PROG: =========
 echo "$PROG: FYI - the expected usage is: $PROG JOB [LOGDIR [CONTEXT]]"
+echo "$PROG: FYI - calculated ARGS are: JOB=$JOB, LOGDIR=$LOGDIR, CONTEXT=$CONTEXT"
+echo "$PROG: FYI - calculated values are: MASTER=$MASTER, SCRIPTSDIR=$SCRIPTSDIR, HADOOP_HOME=$HADOOP_HOME"
 echo $PROG: =========
 
 
@@ -34,15 +56,16 @@ function quit {
 
 # parameters' checks and edits
 [ -n "$1" ] || quit "JOB cannot be empty"
-echo $JOB | grep --silent '^[0-9_]*$' || quit "job can only contains digits and underscore"
-echo $LOGDIR | grep --silent '^/' || LOGDIR=`pwd`/$LOGDIR #rel-path to abs-path
+echo $JOB | grep --silent '^[0-9_]*$' || quit "illegal job id"
 
 
 ################  work starts here ##########################
 
 # ask each slave to collect its JOB logs and scp it to our /tmp
-echo $PROG: running ./bin/slaves.sh, please wait...
-./bin/slaves.sh $SCRIPTSDIR/slave/job-log-collector.sh $MASTER $JOB $LOGDIR $CONTEXT
+echo $PROG: running $HADOOP_HOME/bin/slaves.sh, please wait...
+$HADOOP_HOME/bin/slaves.sh $SCRIPTSDIR/slave/job-log-collector.sh $MASTER $JOB $LOGDIR $CONTEXT
+echo $PROG: running also on master...
+$SCRIPTSDIR/slave/job-log-collector.sh $MASTER $JOB $LOGDIR $CONTEXT # TODO: check if can copy to itself
 
 
 echo $PROG: combining logs, please wait...
