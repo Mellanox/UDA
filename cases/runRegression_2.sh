f1cea17becfe430d7eaa0bc2414cac908b44e1e7
Alon Grinshpoon <alongr@r-zorro004.mtr.labs.mlnx>
2013-10-09 19:10:56 +0300

    issue: 341249 Updated scripts after MARS failed again to run the test
    Change-Id: I5030e6fce2bbf7e08e6d5b3472d5ce19db40adeb

--- cases/runRegression_2.sh
+++ cases/runRegression_2.sh
@@ -5,6 +5,6 @@ echo "Hello from Regression 2!"
 cd /tmp/
 git clone ssh://r-webdev02:29418/accl/uda && cd uda && scp -p -P 29418 r-webdev02:hooks/commit-msg .git/hooks/
 cd /tmp/uda/scripts/regression/
-su eladi -c ./expect.sh performBM_regression.sh /.autodirect/mtrswgwork/eladi/regression/csvs/cluster_conf_1.csv eladi11
+su eladi -c "./expect.sh performBM_regression.sh /.autodirect/mtrswgwork/eladi/regression/csvs/cluster_conf_1.csv eladi11"
 
 echo "SUCCESS!"
