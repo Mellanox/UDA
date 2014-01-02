6671553d302734695f3a5b99239813a28e31c5fd
Elad Itzhakian <eladi@r-zorro012.mtr.labs.mlnx>
2014-01-02 10:22:04 +0200

    issue: 301675. log: updated MARS cases to run from my user.
    Change-Id: I3d127559d570399ed794ae98c7448960dde12536

--- cases/runRegression_2.sh
+++ cases/runRegression_2.sh
@@ -2,7 +2,7 @@
 
 echo "Hello from Regression 2!"
 cd /tmp/mars_tests/UDA-uda.db/tests/scripts/regression/
-su web2ver -c "bash performBM_regression.sh /.autodirect/mtrswgwork/eladi/regression/csvs/cluster_conf_2.csv"
+su eladi -c "bash performBM_regression.sh"
 exitStatus=$?
 if (($exitStatus == 0));then
         echo "SUCCESS!"
