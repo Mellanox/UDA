305879423c9c3cba785c3271640d0222fbf40389
Elad Itzhakian <eladi@r-zorro002.mtr.labs.mlnx>
2014-01-09 17:59:16 +0200

    issue: 341249. log: updated runRegression cases to fit MARS demands
    Change-Id: I637552df77ce847e6c16e78f2b05a481689caafb

--- cases/runRegression_1.sh
+++ cases/runRegression_1.sh
@@ -2,7 +2,7 @@
 
 
 echo "Hello from Regression 1!"
-cd /tmp/mars_tests/UDA-uda.db/tests/scripts/regression/
+cd /tmp/mars_tests/UDA-uda.db/uda_tests/scripts/regression/
 su eladi -c "expect expect.sh performBM_regression.sh /.autodirect/mtrswgwork/eladi/regression/csvs/cluster_conf_1.csv eladi11"
 exitStatus=$?
 if (($exitStatus == 0));then
--- cases/runRegression_2.sh
+++ cases/runRegression_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Hello from Regression 2!"
-cd /tmp/mars_tests/UDA-uda.db/tests/scripts/regression/
+cd /tmp/mars_tests/UDA-uda.db/uda_tests/scripts/regression/
 su web2ver -c "export DAILY_REGRESSION_PREFIX='daily_mars_'; bash performBM_regression.sh"
 exitStatus=$?
 if (($exitStatus == 0));then
