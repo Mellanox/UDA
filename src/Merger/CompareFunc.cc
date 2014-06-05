3a460c35b08270a8629eb9caa606970ec0d9d5ba
Elad Itzhakian <eladi@r-aa-fatty08.mtr.labs.mlnx>
2014-06-05 14:59:22 +0300

    Issue: 395013. Added compare function to support BytesWritable.
    Change-Id: I3cf6b84d0e274db718a8932bedf0dfcc2fdd7d3c

--- src/Merger/CompareFunc.cc
+++ src/Merger/CompareFunc.cc
@@ -19,8 +19,13 @@
 
 #include "CompareFunc.h"
 
+// Matches BytesWritable's definition of LENGTH_BYTES in Java
+const int LENGTH_BYTES = 4;
+
 ////////////////////////////////////////////////////////////////////////////////
-// Use the arrays below for defining whether a given type is a text-comparable, or a byte-comparable (or none of the above)
+// Use the arrays below for defining whether a given type is a 
+// text-comparable/byte-comparable/bytes-comparable (or none of the above)
+//
 const char * TEXT_COMPARABLE[] = {
 		"org.apache.hadoop.io.Text",
 		NULL};
@@ -31,12 +36,14 @@ const char * BYTE_COMPARABLE[] = {
 		"org.apache.hadoop.io.IntWritable",
 		"org.apache.hadoop.io.LongWritable",
 		NULL};
+const char * BYTES_COMPARABLE[] = {
+		"org.apache.hadoop.io.BytesWritable",
+		NULL};
 
 /*
  // unsupported yet - all are derived of WritableComparable
  // see http://hadoop.apache.org/docs/current/api/org/apache/hadoop/io/WritableComparable.html
 
-org.apache.hadoop.io.BytesWritable
 org.apache.hadoop.io.DoubleWritable
 org.apache.hadoop.io.FloatWritable
 
@@ -48,7 +55,7 @@ org.apache.hadoop.mapred.ID (based on int)
 
 
 ////////////////////////////////////////////////////////////////////////////////
-bool strInArray(const char* str, const char *arr[]){
+static bool str_in_array(const char* str, const char *arr[]){
 	for (; *arr; ++arr) {
 		if (strcmp(str,*arr) == 0) {
 			return true;
@@ -57,29 +64,46 @@ bool strInArray(const char* str, const char *arr[]){
 	return false;
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////
-int byte_compare(char* key1, int len1, char* key2, int len2) {
-    int cmp_res =  memcmp(key1, key2, ((len1 < len2) ? len1 : len2));
+static inline int byte_compare_inline(char* key1, int len1, char* key2, int len2) {
+	int cmp_res =  memcmp(key1, key2, ((len1 < len2) ? len1 : len2));
 	return (cmp_res) ? (cmp_res) : (len1 - len2);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int java_text_compare(char* key1, int len1, char* key2, int len2) {
+static int byte_compare(char* key1, int len1, char* key2, int len2) {
+	return byte_compare_inline(key1, len1, key2, len2);
+}
+        
+
+////////////////////////////////////////////////////////////////////////////////
+static int text_compare(char* key1, int len1, char* key2, int len2) {
 	int k1_skip_bytes = StreamUtility::decodeVIntSize((int)(key1[0]));
 	int k2_skip_bytes = StreamUtility::decodeVIntSize((int)(key2[0]));
-	return byte_compare(key1 + k1_skip_bytes, len1 - k1_skip_bytes, key2 + k2_skip_bytes, len2 - k2_skip_bytes);
+	return byte_compare_inline(key1 + k1_skip_bytes, len1 - k1_skip_bytes, key2 + k2_skip_bytes, len2 - k2_skip_bytes);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+static int bytes_compare(char* key1, int len1, char* key2, int len2) {
+	return byte_compare_inline(key1 + LENGTH_BYTES, len1 - LENGTH_BYTES, key2 + LENGTH_BYTES, len2 - LENGTH_BYTES);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 hadoop_cmp_func get_compare_func(const char* java_comparator_type_name) {
 
-	if (strInArray(java_comparator_type_name, TEXT_COMPARABLE)) {
+	if (str_in_array(java_comparator_type_name, TEXT_COMPARABLE)) {
 		log(lsDEBUG, "using Text compare function");
-		return java_text_compare;
+		return text_compare;
 	}
-	else if (strInArray(java_comparator_type_name, BYTE_COMPARABLE)) {
+	else if (str_in_array(java_comparator_type_name, BYTE_COMPARABLE)) {
 		log(lsDEBUG, "using byte compare function");
 		return byte_compare;
+	}
+	else if (str_in_array(java_comparator_type_name, BYTES_COMPARABLE)) {
+		log(lsDEBUG, "using BytesWritable compare function");
+		return bytes_compare;
 	}
 	else {
 		log(lsERROR, "using compare function for unsupported type: '%s'", java_comparator_type_name);
